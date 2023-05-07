b93f5ec95cee161012629a6d7c99eb0c91bdc9e9
S M Hemant <57337914+hem436@users.noreply.github.com>
2023-05-07 08:15:56 +0000

    modified models

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import validates
+from flask_security import UserMixin,RoleMixin,SQLAlchemyUserDatastore
 
 from app import db
-
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
@@ -30,3 +30,12 @@ class Review(db.Model):
 
     def __str__(self):
         return f"{self.user_name}: {self.review_date:%x}"
+
+
+class User(db.Model):
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    username=Column(String(20),nullable=False,unique=True)
+    password=Column(String(20),nullable=False)
+    email = Column(String(100), unique=True)
+    fs_uniquifier = Column(String(255), unique=True, nullable=False)
+    admin= Column(Boolean,default=False)
