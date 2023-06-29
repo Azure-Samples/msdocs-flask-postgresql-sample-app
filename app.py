802c484074a1a6653a71071e5dde3a11442877ea
SvartAlfe <35220922+SvartAlfe@users.noreply.github.com>
2023-06-29 13:59:14 +0200

    insert/init mess

--- app.py
+++ app.py
@@ -40,14 +40,14 @@ def livecheck():
 @app.route('/insertintodb', methods=['GET'])
 def insertintodb():
     try:
-        return qnainit()
+        return InsertQnA()
     except:
         return 'An exception occurred'
 
 @app.route('/qnainit', methods=['GET'])
 def qnainit():
     try:
-        return InsertQnA()
+        return qnainit()
     except:
         return 'An exception occurred'
 
@@ -66,22 +66,22 @@ def qnainit():
         openai.api_type = os.getenv("OPENAI_TYPE")
         openai.api_version = os.getenv("OPENAI_VERSION")
         
-        # with open('./data/qna.csv', newline='\r\n') as srccsvfile:
-        #     datareader = csv.reader(srccsvfile, delimiter=';')
+        with open('./data/qna.csv', newline='\r\n') as srccsvfile:
+             datareader = csv.reader(srccsvfile, delimiter=';')
             
-        #     with open('./embeddings/embeddings.csv', 'a', newline='\r\n') as dstcsvfile:
-        #         tmpwriter = csv.writer(dstcsvfile, delimiter=';', quotechar='', quoting=csv.QUOTE_NONE)
-        #         for row in datareader:
-        #             question = row[0]
-        #             answer = row[1]
+             with open('./embeddings/embeddings.csv', 'a', newline='\r\n') as dstcsvfile:
+                 tmpwriter = csv.writer(dstcsvfile, delimiter=';', quotechar='', quoting=csv.QUOTE_NONE)
+                 for row in datareader:
+                     question = row[0]
+                     answer = row[1]
                     
-        #             response = openai.Embedding.create(
-        #                 input=question,
-        #                 engine=os.getenv("OPENAI_DEPLOYMENT_EMBEDDING")
-        #             )
-        #             embeddings = response['data'][0]['embedding']
-        #             logs += '\r\n\r\n' + question + ' ' + embeddings
-        #             tmpwriter.writerow([question,answer,embeddings])
+                     response = openai.Embedding.create(
+                         input=question,
+                         engine=os.getenv("OPENAI_DEPLOYMENT_EMBEDDING")
+                     )
+                     embeddings = response['data'][0]['embedding']
+                     logs += '\r\n\r\n' + question + ' ' + embeddings
+                     tmpwriter.writerow([question,answer,embeddings])
         return 'success' + logs
     except:
         return 'error again in qnainit'
