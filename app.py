addae7bacf905454076dfadcf88296e365e87d78
SvartAlfe <35220922+SvartAlfe@users.noreply.github.com>
2023-06-29 14:49:09 +0200

    test

--- app.py
+++ app.py
@@ -67,22 +67,22 @@ def do_init():
         openai.api_version = os.getenv("OPENAI_VERSION")
         
         with open('./data/qna.csv', newline='\r\n') as srccsvfile:
-             datareader = csv.reader(srccsvfile, delimiter=';')
+            datareader = csv.reader(srccsvfile, delimiter=';')
             
-             with open('./embeddings/embeddings.csv', 'a', newline='\r\n') as dstcsvfile:
-                 tmpwriter = csv.writer(dstcsvfile, delimiter=';', quotechar='', quoting=csv.QUOTE_NONE)
-                 for row in datareader:
-                     question = row[0]
-                     answer = row[1]
-                    
-                     response = openai.Embedding.create(
-                         input=question,
-                         engine=os.getenv("OPENAI_DEPLOYMENT_EMBEDDING")
-                     )
-                     embeddings = response['data'][0]['embedding']
-                     logs += '\r\n\r\n' + question + ' ' + embeddings
-                     tmpwriter.writerow([question,answer,embeddings])
-                     InsertQnA(question, embeddings, answer)
+            #  with open('./embeddings/embeddings.csv', 'a', newline='\r\n') as dstcsvfile:
+            #      tmpwriter = csv.writer(dstcsvfile, delimiter=';', quotechar='', quoting=csv.QUOTE_NONE)
+            for row in datareader:
+                question = row[0]
+                answer = row[1]
+            
+                response = openai.Embedding.create(
+                    input=question,
+                    engine=os.getenv("OPENAI_DEPLOYMENT_EMBEDDING")
+                )
+                embeddings = response['data'][0]['embedding']
+                logs += '\r\n\r\n' + question + ' ' + embeddings
+                tmpwriter.writerow([question,answer,embeddings])
+                InsertQnA(question, embeddings, answer)
         return 'success' + logs
     except:
         return 'error again in qnainit'
