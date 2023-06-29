7c0a8f244832480f1a8e67b1407ed7a3fb8bb61e
SvartAlfe <35220922+SvartAlfe@users.noreply.github.com>
2023-06-29 12:52:05 +0200

    clecnup

--- app.py
+++ app.py
@@ -32,100 +32,17 @@ db = SQLAlchemy(app)
 # Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
 migrate = Migrate(app, db)
 
-# The import must be done after db initialization due to circular import issue
-from models import Restaurant, Review
-
-@app.route('/', methods=['GET'])
-def index():
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()
-    return render_template('index.html', restaurants=restaurants)
-
-@app.route('/<int:id>', methods=['GET'])
-def details(id):
-    restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant == id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews)
-
-@app.route('/create', methods=['GET'])
-def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
-
-@app.route('/prompt/<prompt>', methods=['GET'])
-def qna_prompt(prompt):
-    print('qna_prompt')
-    return prompt
+@app.route('/livecheck', methods=['GET'])
+def livecheck():
+    return 'OK'
 
 @app.route('/qnainit', methods=['GET'])
 def qnainit():
     try:
-        return await qnainit.qnainit()
+        return qnainit.qnainit()
     except:
         return 'An exception occurred'
 
-@app.route('/add', methods=['POST'])
-@csrf.exempt
-def add_restaurant():
-    try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
-    except (KeyError):
-        # Redisplay the question voting form.
-        return render_template('add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
-        })
-    else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        db.session.add(restaurant)
-        db.session.commit()
-
-        return redirect(url_for('details', id=restaurant.id))
-
-@app.route('/review/<int:id>', methods=['POST'])
-@csrf.exempt
-def add_review(id):
-    try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')
-    except (KeyError):
-        #Redisplay the question voting form.
-        return render_template('add_review.html', {
-            'error_message': "Error adding review",
-        })
-    else:
-        review = Review()
-        review.restaurant = id
-        review.review_date = datetime.now()
-        review.user_name = user_name
-        review.rating = int(rating)
-        review.review_text = review_text
-        db.session.add(review)
-        db.session.commit()
-
-    return redirect(url_for('details', id=id))
-
-@app.context_processor
-def utility_processor():
-    def star_rating(id):
-        reviews = Review.query.where(Review.restaurant == id)
-
-        ratings = []
-        review_count = 0
-        for review in reviews:
-            ratings += [review.rating]
-            review_count += 1
-
-        avg_rating = sum(ratings) / len(ratings) if ratings else 0
-        stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
-
-    return dict(star_rating=star_rating)
 
 @app.route('/favicon.ico')
 def favicon():
--- qnainit.py
+++ qnainit.py
@@ -6,13 +6,12 @@ def qnainit():
     print('Entering qnainit')
     
     logs = "logs:"
-    
+
     try:
         openai.api_key = os.getenv("OPENAI_API_KEY")
         openai.api_base = os.getenv("OPENAI_ENDPOINT")
         openai.api_type = os.getenv("OPENAI_TYPE")
         openai.api_version = os.getenv("OPENAI_VERSION")
-
         
         with open('./data/qna.csv', newline='\r\n') as srccsvfile:
             datareader = csv.reader(srccsvfile, delimiter=';')
