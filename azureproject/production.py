ec5a28509c4d7456253044c6ce0fa3920cbc55fd
almokashfy <39690861+almokashfy@users.noreply.github.com>
2025-02-16 17:44:12 +0000

    Configure Azure database connecton

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,8 +1,8 @@
 import os
 
 # DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=os.getenv('Almokashfy'),
-#     dbpass=os.getenv('Thulah@1822'),
-#     dbhost=os.getenv('erp-server.postgres.database.azure.com'),
-#     dbname=os.getenv('erp-server')
+#     dbuser=os.getenv('AZURE_POSTGRESQL_USER', 'Almokashfy'),
+#     dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD', 'Thulah@1822'),
+#     dbhost=os.getenv('AZURE_POSTGRESQL_HOST', 'erp-server.postgres.database.azure.com'),
+#     dbname=os.getenv('AZURE_POSTGRESQL_NAME', 'erp-server')
 # )
