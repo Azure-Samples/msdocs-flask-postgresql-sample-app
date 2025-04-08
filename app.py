fa080a7f39b13f419ad588e6473553f16f7bf857
krivamar <85482111+krivamar@users.noreply.github.com>
2025-04-08 19:41:22 +0000

    #port 8000

--- app.py
+++ app.py
@@ -119,5 +119,5 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 # add port 8000
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+#if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=8000)
