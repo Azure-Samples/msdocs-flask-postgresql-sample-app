d1ad5bc1ff09d47ec669628cf480e25e4a5d4ef5
Carlos Jiménez Crespo <151526546+cjimenezcuah@users.noreply.github.com>
2025-04-17 13:45:16 +0000

    Configure Azure database connection

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-
+# This is the main entry point for the application.
 app = Flask(__name__, static_folder='static')
 csrf = CSRFProtect(app)
 
